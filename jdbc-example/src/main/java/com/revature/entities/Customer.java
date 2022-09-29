4b4ada5da5869319eb1fc2c0077aeb7f306c158e
EricTrainingRev <eric.suminski@revature.com>
2022-09-29 14:07:51 -0400

    adding entity info

---
+++ jdbc-example/src/main/java/com/revature/entities/Customer.java
@@ -0,0 +1,32 @@
+package com.revature.entities;
+
+public class Customer {
+    public int userId;
+    public String firstName;
+    public String lastName;
+    public String username;
+    public String pass;
+
+    public Customer() {}
+
+    public Customer(int userId, String firstName, String lastName, String username, String pass) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
+
+
+}
